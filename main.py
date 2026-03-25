16aa9bc0f043fc79402728766c91971292f83987
pipeline <pipeline@example.com>
2026-03-25 23:19:07 +0100

    keep

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Hello, VS Code Python Project!")
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+#from tavily import TravlyClient
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+
+    Args:
+        query (str): The query to search for
+
+    Returns:
+        The search result.
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny."
+llm = ChatOpenAI()
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+def main():
+    print("Hello, VS Code Python Project!")
+    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokoyo?")})
+    
+if __name__ == "__main__":
+    main()

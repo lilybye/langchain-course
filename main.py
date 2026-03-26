ec6643cc006dffba54c45f4a6553483e2c1024b4
pipeline <pipeline@example.com>
2026-03-26 20:54:33 +0100

    final check

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,9 +8,12 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-#from tavily import TravlyClient
+from tavily import TravlyClient
+from langchain_tavily import TavilyResearch
 
-@tool
+tavily = TravlyClient()
+
+@tool  # custom tool from vendor
 def search(query: str) -> str:
     """
     Tool that searches over internet
@@ -20,10 +25,26 @@ def search(query: str) -> str:
         The search result.
     """
     print(f"Searching for {query}")
-    return "Tokyo weather is sunny."
-llm = ChatOpenAI()
-tools = [search]
-agent = create_agent(model=llm, tools=tools)
+    return tavily.search(query=query)
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    
+    url:str = Field(description="The URL of source")
+    
+class AgentReponse(BaseModel):
+    """Schema fo agent response with answer and sources"""
+    
+    answer:str = Field(description="The agent's answer to the query")
+    source: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer.")
+
+
+llm = ChatOpenAI(model="gpt-5")
+#tools = [search]
+tools = [TavilyResearch()]
+#agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentReponse)
+
 def main():
     print("Hello, VS Code Python Project!")
     result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokoyo?")})

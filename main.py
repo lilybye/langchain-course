b39774be322166060fb04b4404e4c17a98e855a1
pipeline <pipeline@example.com>
2026-04-21 23:25:08 +0200

    complete langgraph agent

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from graph import app
+
 if __name__ == "__main__":
     print("Hello Advanced RAG")
+    print(app.invoke(input={"question": "what is agent memory?"}))
